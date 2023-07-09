00069e6f4418a2f0172e30d0ec48c6b0cfd61a7e
notchuks <chukaezem@gmail.com>
2023-07-09 01:14:44 +0100

    first commit

---
+++ components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Socials from "../Socials";
+import Link from "next/link";
+import Button from "../Button";
+
+const Footer = ({}) => {
+  return (
+    <>
+      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+        Made With ❤ by{" "}
+        <Link href="http://www.chetanverma.com">
+          <a className="underline underline-offset-1">Chukwuka Ezem</a>
+        </Link>
+      </h1>
+    </>
+  );
+};
+
+export default Footer;
---
+++ pages/index.js
@@ -0,0 +1,104 @@
+import { useRef } from "react";
+import Header from "../components/Header";
+import ServiceCard from "../components/ServiceCard";
+import Socials from "../components/Socials";
+import WorkCard from "../components/WorkCard";
+import { useIsomorphicLayoutEffect } from "../utils";
+import { stagger } from "../animations";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import Button from "../components/Button";
+import Link from "next/link";
+import Cursor from "../components/Cursor";
+
+// Local Data
+import data from "../data/portfolio.json";
+import Resume from "./resume";
+
+export default function Home() {
+  // Ref
+  const workRef = useRef();
+  const aboutRef = useRef();
+  const textOne = useRef();
+  const textTwo = useRef();
+  const textThree = useRef();
+  const textFour = useRef();
+
+  // Handling Scroll
+  const handleWorkScroll = () => {
+    window.scrollTo({
+      top: workRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleAboutScroll = () => {
+    window.scrollTo({
+      top: aboutRef.current.offsetTop,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useIsomorphicLayoutEffect(() => {
+    stagger(
+      [textOne.current, textTwo.current, textThree.current, textFour.current],
+      { y: 40, x: -10, transform: "scale(0.95) skew(10deg)" },
+      { y: 0, x: 0, transform: "scale(1)" }
+    );
+  }, []);
+
+  return (
+    <div className={`relative ${data.showCursor && "cursor-none"}`}>
+      {data.showCursor && <Cursor />}
+      <Head>
+        <title>{data.name}</title>
+      </Head>
+
+      <div className="gradient-circle"></div>
+      <div className="gradient-circle-bottom"></div>
+
+      <div className="container mx-auto mb-10">
+        <Header
+          handleWorkScroll={handleWorkScroll}
+          handleAboutScroll={handleAboutScroll}
+        />
+        <div className="laptop:mt-20 mt-10">
+          <div className="mt-5">
+            <h1
+              ref={textOne}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+            >
+              {data.headerTaglineOne}
+            </h1>
+            <h1
+              ref={textTwo}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineTwo}
+            </h1>
+            <h1
+              ref={textThree}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineThree}
+            </h1>
+            <h1
+              ref={textFour}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+            >
+              {data.headerTaglineFour}
+            </h1>
+          </div>
+
+          <Socials className="mt-2 laptop:mt-5" />
+        </div>
+
+        <Resume />
+        
+        <Footer />
+      </div>
+    </div>
+  );
+}
